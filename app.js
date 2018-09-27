9e7419289a65b1d406d2275859827a036100645b
Heer Ruben <rh064@hdm-stuttgart.de>
2018-09-27 14:30:53 +0200

    session

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var builder = require('botbuilder');
 var AdaptiveCards = require("adaptivecards");
 var confirmed = true;
 
+var Promise = require('bluebird');
+var data = require('./data.json');
+
 var idCard = require("./cards/id-card.json");
 var selectionCard = require("./cards/selection-card.json");
 
@@ -23,18 +26,57 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 
-var bot = new builder.UniversalBot(connector, function (session) {
-    if (session.message && session.message.value) {
-        processSubmitAction(session, session.message.value);
-        var confirmed = false;
-        return;   
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Wilkommen.");
+        session.beginDialog('askForID');
+    },
+    function (session, results) {
+        session.dialogData.ID = results.response;
+        var job = getJob(`${session.dialogData.ID}`);
+        session.send(job.MieterName);
+        session.beginDialog('askForMore')
+    },
+    function (session) {
+        //session.send("Weiter gehts");
     }
+        ]).set('storage', inMemoryStorage); // Register in-memory storage 
 
-    var msg = new builder.Message(session).addAttachment(idCard);
-    session.send(msg);    
-});
-bot.dialog('id-search', require('./id-search'));
+    bot.dialog('askForID', [
+        function (session) {
+            builder.Prompts.text(session, "Bitte Auftragsnummer eingeben");
+        },
+        function (session, results) {
+            session.endDialogWithResult(results);
+        }
+    ]);
 
+    bot.dialog('askForMore', [
+        function (session) {
+            session.send("Weiter gehts");
+            session.endDialog();
+        }
+    ]);
+
+    // msg = new builder.Message(session).addAttachment(idCard);
+    // session.send(msg);
+
+    //bot.dialog('id-search', require('./id-search'));
+
+function getJob(id) {
+    var jobAuftrag;
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                var job = data[key]
+                if(job.AuftragNr === id) {
+                    jobAuftrag = job
+                }
+            }
+        }
+        return jobAuftrag;
+}
 
 function processSubmitAction(session, value) {
     var defaultErrorMessage = 'Bitte geben Sie eine Auftragsnummer ein';
