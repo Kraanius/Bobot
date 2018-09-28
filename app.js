dc2a98ddfebb4c5c1690ff2c9c7e4056840ef036
Andreas Riedel <ar113@hdm-stuttgart.de>
2018-09-28 11:43:20 +0200

    Stornieren und verschieben

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var AdaptiveCards = require("adaptivecards");
-
+var nodemailer = require('nodemailer');
+const fs = require('fs');
 var data = require('./data.json');
 var job;
 
 var idCard = require("./cards/id-card.json");
 var selectionCard = require("./cards/selection-card.json");
+var dateCard = require("./cards/dateCard.json");
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -38,70 +40,79 @@ var bot = new builder.UniversalBot(connector, [
         console.log('###2');
         session.dialogData.ID = results.response;
         job = getJob(`${session.dialogData.ID}`);
-        var damageCard = {
-            "contentType": "application/vnd.microsoft.card.adaptive",
-            "content": {
-            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-            "type": "AdaptiveCard",
-            "version": "1.0",
-            "body": [
-                {
-                    "type": "Container",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "Auftragsnummer: " + job.AuftragNr,
-                            "weight": "bolder",
-                            "size": "medium"
-                        },
-                        {
-                            "type": "ImageSet",
-                            "imageSize": "medium",
-                            "images": [
-                                {
-                                    "type": "Image",
-                                    "url": "http://www.maler-wenzel.de/_res/metall/064_Heizkoerper/IMG_0967.jpg"
-                                },
-                                {
-                                    "type": "Image",
-                                    "url": "http://www.maler-wenzel.de/_res/metall/064_Heizkoerper/IMG_1008.jpg"
-                                },
-                                {
-                                    "type": "Image",
-                                    "url": "http://www.maler-wenzel.de/_res/metall/064_Heizkoerper/IMG_1007.jpg"
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "type": "Container",
-                    "items": [
-                        {
-                            "type": "FactSet",
-                            "facts": [
-                                {
-                                    "title": "Name:",
-                                    "value": job.MieterName
-                                },
-                                {
-                                    "title": "Datum:",
-                                    "value": job.TerminDatum_absolut
-                                },
-                                {
-                                    "title": "Schaden",
-                                    "value": job.Inventar + " " + job.Schaden
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
+        var msg = "Wir können diesen Auftrag leider nicht finden."
+        if(job !== null) {
+            var damageCard = {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": {
+                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                "type": "AdaptiveCard",
+                "version": "1.0",
+                "body": [
+                    {
+                        "type": "Container",
+                        "items": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Auftragsnummer: " + job.AuftragNr,
+                                "weight": "bolder",
+                                "size": "medium"
+                            },
+                            {
+                                "type": "ImageSet",
+                                "imageSize": "medium",
+                                "images": [
+                                    {
+                                        "type": "Image",
+                                        "url": "http://www.maler-wenzel.de/_res/metall/064_Heizkoerper/IMG_0967.jpg"
+                                    },
+                                    {
+                                        "type": "Image",
+                                        "url": "http://www.maler-wenzel.de/_res/metall/064_Heizkoerper/IMG_1008.jpg"
+                                    },
+                                    {
+                                        "type": "Image",
+                                        "url": "http://www.maler-wenzel.de/_res/metall/064_Heizkoerper/IMG_1007.jpg"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "Container",
+                        "items": [
+                            {
+                                "type": "FactSet",
+                                "facts": [
+                                    {
+                                        "title": "Name:",
+                                        "value": job.MieterName
+                                    },
+                                    {
+                                        "title": "Datum:",
+                                        "value": job.TerminDatum_absolut
+                                    },
+                                    {
+                                        "title": "Schaden",
+                                        "value": job.Inventar + " " + job.Schaden
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+            }
+            var msg = new builder.Message(session).addAttachment(damageCard);
         }
-        }
-        var msg = new builder.Message(session).addAttachment(damageCard);
+        
         session.send(msg);   
-        session.beginDialog('askForMore')
+        if(job !== null) {
+            session.beginDialog('askForMore')
+        } else {
+            session.beginDialog('askForID')
+        }
+        
     },
 ]).set('storage', inMemoryStorage); // Register in-memory storage 
 
@@ -111,7 +122,7 @@ var bot = new builder.UniversalBot(connector, [
             // var msg = new builder.Message(session).addAttachment(idCard);
             // session.send(msg);   
 
-            builder.Prompts.text(session, "Bitte geben Sie ihre Auftragsnummer ein.");
+            builder.Prompts.number(session, "Bitte geben Sie ihre Auftragsnummer ein.");
 
         },
         function (session, results) {
@@ -121,28 +132,18 @@ var bot = new builder.UniversalBot(connector, [
         }
     ]);
 
-    bot.dialog('askForMore',
-        function (session) {
-            console.log('###6');
-            if(session.message && session.message.value) {
-                console.log('###7');
-                console.log(session.message.value)
-                processSubmitAction(session, session.message.value);
-                return;
-            }
-            var msg = new builder.Message(session).addAttachment(selectionCard);
-            session.send(msg);    
 
-        }
-    );
 
-    function getJob(id) {
-    var jobAuftrag;
+function getJob(id) {
+    var jobAuftrag = null;
         for (var key in data) {
             if (data.hasOwnProperty(key)) {
                 var job = data[key]
                 if(job.AuftragNr === id) {
-                    jobAuftrag = job
+                    if(job.TerminStatus !== 'storniert' ){
+                        jobAuftrag = job
+                    }
+                    
                 }
             }
         }
@@ -156,10 +157,15 @@ function deleteJob(id) {
             var job = data[key]
             if(job.AuftragNr === id) {
                 job.TerminStatus = "storniert";
-                console.log('###12', job.TerminStatus);   
+                break;
             }
         }
     }
+    console.log("###1000", data)
+    let data2 = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', data2, (err) => {  
+        if (err) throw err;
+    });
 }
 
 
@@ -173,23 +179,21 @@ function processSubmitAction(session, value) {
         break;
         case 'picture':
             session.beginDialog('takePicture');
+        case 'date':
+            session.beginDialog('changeDate');
         break;
     }
 }
 
 bot.dialog('deleteAppointment', [
     function (session) {
-        console.log('###9');
         builder.Prompts.confirm(session, "Sind Sie sicher, dass sie den Termin löschen wollen?");
     }, function(session, result) {
-        console.log('###10');
-        console.log(result.response)
-        console.log(job.AuftragNr)
         if(result.response) {
             deleteJob(job.AuftragNr)
             session.send(`Ihr Termin mit der Auftragsnummer ${job.AuftragNr} wurde aus unserem System gelöscht.`);
         } else {
-            session.send("Termin wurde nicht gelöscht gelöscht!");
+            session.send("Termin wurde nicht gelöscht!");
             session.beginDialog('askForMore');
         }     
         session.endDialog();
@@ -198,7 +202,55 @@ bot.dialog('deleteAppointment', [
 
 bot.dialog('moveAppointment', [
     function (session) {
-        session.send("Termin wurde verschoben!");
-        session.endDialog();
+        if(session.message && session.message.value.type === "date") {
+            submitChangeDate(session, session.message.value)
+            return;
+        }
+        var msg = new builder.Message(session).addAttachment(dateCard);
+        session.send(msg); 
+        
     }
 ]);
+
+bot.dialog('askForMore',
+function (session) {  
+    var msg = new builder.Message(session).addAttachment(selectionCard);
+    session.send(msg);  
+    if(session.message && session.message.value) {
+        console.log('###7');
+        console.log(session.message.value)
+        processSubmitAction(session, session.message.value);
+        return;
+    }
+   
+
+});
+
+function submitChangeDate(session, value){
+    date = value.DateVal
+    session.beginDialog('changeDate1',{date});
+}
+
+bot.dialog('changeDate1',function (session, date) {
+    console.log('###6', date.date);    
+    let changedDate = changeDateInJson(date.date)
+    session.send(`Ihr Termin wurde auf den ${changedDate} verschoben.`)
+    session.endDialog();
+});
+
+function changeDateInJson(date){
+for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+        var jobs = data[key]
+        if(jobs.AuftragNr === job.AuftragNr) {
+            jobs.TerminDatum_absolut = date
+        }
+    }
+}
+
+let data3 = JSON.stringify(data, null, 2);
+fs.writeFile('data.json', data3, (err) => {  
+    if (err) throw err;
+});
+return date;
+}
